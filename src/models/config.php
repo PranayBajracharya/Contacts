213c361ce4d63381735eb1d5a0eb5386fbc52f0f
PranayBajracharya <law.pran99@gmail.com>
2022-01-31 22:58:54 +0545

    feat: added db config

---
+++ src/models/config.php
@@ -0,0 +1,17 @@
+<?php
+
+class Config {
+    private $host = 'localhost';
+    private $user = 'root';
+    private $password = '';
+    private $dbname = 'contacts';
+
+    protected function connect() {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $db = new PDO($dsn, $this->user, $this->password);
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db;
+    }
+}
+
+?>
