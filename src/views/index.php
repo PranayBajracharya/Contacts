cdab7217fd97e95ac292ab4d6784f4965552bf54
PranayBajracharya <law.pran99@gmail.com>
2022-02-02 00:07:31 +0545

    feat: index view according to db

--- src/views/index.php
+++ src/views/index.php
@@ -6,14 +6,7 @@
 <html lang="en">
     <link rel="stylesheet" href="../../public/css/index.css">
     <?php include('./header.php'); ?>
-    <main>
-        <?php 
-            $testObject2 = new IndexController();
-            $results = $testObject2->index();
-            foreach ($results as $data) {
-                echo $data['id'] . ' ' . $data['first_name'] . ' ' . $data['last_name'] . ' ' . $data['nick_name'] . ' ' . $data['email'] . '<br>';
-            }
-        ?> 
+    <main>   
         <div class="table">
             <div class="thead">
                 <div class="name">Name</div>
@@ -23,99 +16,41 @@
                 <!-- <div class="row">
                 </div> -->
             </div>
-            <div class="tbody">                
-                <div class="row">
-                    <div class="name">
-                        <div class="profile circle">
-                            <img src="../../public/img/toothless.png" alt="profile picture" class="circle">
+            <div class="tbody">
+            <?php 
+                $testObject2 = new IndexController();
+                $results = $testObject2->index();
+                foreach ($results as $data) {
+            ?>
+                    <div class="row">
+                        <div class="name">
+                            <div class="profile circle">
+                                <img src="../../public/img/toothless.png" alt="profile picture" class="circle">
+                            </div>
+                            <div class="full-name">
+                                <span><?php echo $data['first_name'] . ' ' . $data['last_name'] ?></span> 
+                            </div>
                         </div>
-                        <div class="full-name">
-                            <span>Pranay Bajracharya</span> 
-                        </div>
+                        <div class="email"><?php echo $data['email'] ?></div>
+                        <div class="phone"><?php echo $data['phone'] ?></div>
+                        <div class="buttons">
+                            <button class="button">
+                                <img src="../../public/img/star.png" alt="">
+                            </button>
+                            <button class="button">
+                                <img src="../../public/img/pencil.png" alt="">
+                            </button>
+                            <button class="button">
+                                <img src="../../public/img/dustbin.png" alt="">
+                            </button>
+                        </div>   
                     </div>
-                    <div class="email"></div>
-                    <div class="phone">9861964711</div>
-                    <div class="buttons">
-                        <button class="button">
-                            <img src="../../public/img/star.png" alt="">
-                        </button>
-                        <button class="button">
-                            <img src="../../public/img/pencil.png" alt="">
-                        </button>
-                        <button class="button">
-                            <img src="../../public/img/dustbin.png" alt="">
-                        </button>
-                    </div>   
-                </div>            
-                <div class="row">
-                    <div class="name">
-                        <div class="profile circle">
-                            <!-- <img src="../../public/img/toothless.png" alt="profile picture" class="circle"> -->
-                            <span class="circle">PB</span>
-                        </div>
-                        <div class="full-name">
-                            <span>Pranay Bajracharya</span> 
-                        </div>                    </div>
-                    <div class="email">law.asdasdasdpran99@gmail.com</div>
-                    <div class="phone">9812312323</div>
-                    <div class="buttons">
-                        <button class="button">
-                            <img src="../../public/img/star.png" alt="">
-                        </button>
-                        <button class="button">
-                            <img src="../../public/img/pencil.png" alt="">
-                        </button>
-                        <button class="button">
-                            <img src="../../public/img/dustbin.png" alt="">
-                        </button>
-                    </div>   
-                </div>               
-                <div class="row">
-                    <div class="name">
-                        <div class="profile circle">
-                            <img src="../../public/img/toothless.png" alt="profile picture" class="circle">
-                        </div>
-                        <div class="full-name">
-                            <span>Pranay Bajracharya</span> 
-                        </div>                    </div>
-                    <div class="email">law.pran99@gmail.com</div>
-                    <div class="phone"></div>
-                    <div class="buttons">
-                        <button class="button">
-                            <img src="../../public/img/star.png" alt="">
-                        </button>
-                        <button class="button">
-                            <img src="../../public/img/pencil.png" alt="">
-                        </button>
-                        <button class="button">
-                            <img src="../../public/img/dustbin.png" alt="">
-                        </button>
-                    </div>   
-                </div>               
-                <div class="row">
-                    <div class="name">
-                        <div class="profile circle">
-                            <img src="../../public/img/toothless.png" alt="profile picture" class="circle">
-                        </div>
-                        <div class="full-name">
-                            <span>Pranay Bajracharya</span> 
-                        </div>                    </div>
-                    <div class="email">law.pran99@gmail.com</div>
-                    <div class="phone">123123123</div>
-                    <div class="buttons">
-                        <button class="button">
-                            <img src="../../public/img/star.png" alt="">
-                        </button>
-                        <button class="button">
-                            <img src="../../public/img/pencil.png" alt="">
-                        </button>
-                        <button class="button">
-                            <img src="../../public/img/dustbin.png" alt="">
-                        </button>
-                    </div>   
-                </div>   
             </div>
+            <?php
+                }                
+            ?> 
         </div>
+        
     </main>
 </body>
 </html>
